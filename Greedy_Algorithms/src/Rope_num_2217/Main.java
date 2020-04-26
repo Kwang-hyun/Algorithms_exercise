bea7120df02614c3d331f2ff15b2222011807bcc
Choikwanghyun <Choikwanghyun@LAPTOP-4PMFOAAG>
2020-04-26 22:06:38 +0900

    complete

--- Greedy_Algorithms/src/Rope_num_2217/Main.java
+++ Greedy_Algorithms/src/Rope_num_2217/Main.java
@@ -1,7 +1,36 @@
 package Rope_num_2217;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
+	
+	private static ArrayList<Integer> list = new ArrayList<Integer>();
+	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		
+		int N = sc.nextInt();
+		int temp = 0;
+	
+		for(int i = 0; i < N; i++) {
+			int weight = sc.nextInt();
+			list.add(weight);
+		}
+		
+		Collections.sort(list, Collections.reverseOrder());
+		
+		int Max = list.get(0);
+		
+		for(int i = 0; i < N; i++ ) {
+			temp = (i+1) * list.get(i);
+			if(temp > Max) {
+				Max = temp;
+			}
+		}
+		
+		System.out.println(Max);
 	}
 }
